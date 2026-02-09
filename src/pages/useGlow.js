ccea3b137f5322d6543a630590613f287cb1f1e8
Abhay Kumar <your-email@example.com>
2026-02-09 13:37:53 +0530

    Made a glow effect on hover

---
+++ src/pages/useGlow.js
@@ -0,0 +1,10 @@
+export function handleGlow(e) {
+  const card = e.currentTarget;
+  const rect = card.getBoundingClientRect();
+
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  card.style.setProperty("--x", `${x}px`);
+  card.style.setProperty("--y", `${y}px`);
+}
